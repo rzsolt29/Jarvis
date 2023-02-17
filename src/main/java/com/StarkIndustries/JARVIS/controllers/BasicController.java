7afd127e446249e54a343b0fc51e1412007ec854
Révész Zsolt <revzsolt29@gmail.com>
2023-02-17 09:56:20 +0100

    Added welcome page

---
+++ src/main/java/com/StarkIndustries/JARVIS/controllers/BasicController.java
@@ -0,0 +1,21 @@
+package com.StarkIndustries.JARVIS.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BasicController {
+    private final Logger logger = LoggerFactory.getLogger(BasicController.class);
+
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        logger.debug("request to GET index");
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+
+}
