f4819baffe426b46163c9a52f2434598c7928ca9
Zsolt Révész <rzsolt29@gmail.com>
2023-02-18 11:57:31 +0100

    Some CSS

--- src/main/java/com/StarkIndustries/JARVIS/controllers/BasicController.java
+++ src/main/java/com/StarkIndustries/JARVIS/controllers/BasicController.java
@@ -1,5 +1,8 @@
 package com.StarkIndustries.JARVIS.controllers;
 
+import java.time.Instant;
+import java.time.ZoneId;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -15,6 +18,7 @@ public class BasicController {
     public ModelAndView index(){
         logger.debug("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("today", Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek());
         return modelAndView;
     }
 
