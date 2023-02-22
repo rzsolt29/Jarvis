460e2aef4477a0598ce3c6ce0b3fc0f37d512630
Révész Zsolt <revzsolt29@gmail.com>
2023-02-22 16:48:11 +0100

    Rename file

--- src/main/java/com/StarkIndustries/JARVIS/controllers/FormController.java
+++ src/main/java/com/StarkIndustries/JARVIS/controllers/FormController.java
@@ -1,17 +1,18 @@
 package com.StarkIndustries.JARVIS.controllers;
 
+import org.springframework.stereotype.Controller;
+
 import java.time.Instant;
 import java.time.ZoneId;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class BasicController {
-    private final Logger logger = LoggerFactory.getLogger(BasicController.class);
+public class FormController {
+    private final Logger logger = LoggerFactory.getLogger(FormController.class);
 
 
     @GetMapping("/")
@@ -21,5 +22,4 @@ public class BasicController {
         modelAndView.addObject("today", Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek());
         return modelAndView;
     }
-
 }
